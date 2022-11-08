b4c93a339067c93c697f49ccf5c58ba6ec54138f
namanama10 <117552889+namanama10@users.noreply.github.com>
2022-11-08 18:23:57 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+/** STUDY CASE 1 (mencari luas segitiga)
+ * 
+ * 
+ * buat algoritma dan pseucode untuk luas segitiga 
+ * 
+ * ALGORITMA
+ * 1. buat var alas
+ * 2. buat var tinggi
+ * 3. buat var luas
+ * 4. masukan var luas dengan rumus 1/2 * alas * tinggi
+ * 5. tampilkan luas
+ * 
+ * PSEUDOCODE
+ * ISI alas DENGAN ANGKA
+ * ISI tinggi DENGAN ANGKA
+ * ISI luas DENGAN 1/2 * alas * tinggi
+ * CETAK luas (console.log)
+ */
+
+//CODING
+let alas = 10;
+let tinggi = 5;
+let luas = (1/2) * alas * tinggi;
+console.log(luas);
+
+
+/** STUDY CASE 2 (mencari luas lingkaran)
+ * 
+ * ALGORITMA
+ * 1. buat var ruas
+ * 2. buat var luas_lingkaran
+ * 3. masukan var luas_lingkaran dengan rumus (22/7) * ruas * ruas
+ * 4. tampilkan luas_lingkaran (console.log)
+ * 
+ * 
+ * PSEUDOCODE
+ * PSEUDOCODE
+ * ISI ruas DENGAN ANGKA
+ * ISI luas_lingkaran DENGAN (24/7) * ruas^2
+ * CETAK luas_lingkaran (console.log)
+ */
+
+
+let ruas = 10;
+let luas_lingkaran = (22/7) * ruas * ruas;
+console.log(luas_lingkaran);
+
+
+/** STUDY CASE 3 
+ * 
+ * hitung volume bola, buat algoritma dan pseudocode
+ * 
+ * ALGORITMA 
+ * 1. buat var ruas (r)
+ * 2. buat var volume_bola
+ * 3. masukan var volume_bola dengan rumus (4/3) * 3.14 * r^3
+ * tampilkan volume_bola
+ *  
+ * PSEUDOCODE
+ * ISI r DENGAN ANGKA
+ * ISI volume_bola DENGAN (4/3) * 3.14 * r^3
+ * CETAK volume_bola (console.log)
+ */
+
+let r = 5**3;
+let volume_bola = (4/3) * 3.14 * r;
+console.log(volume_bola);
